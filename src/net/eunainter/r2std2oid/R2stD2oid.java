946d74b82058237abe2ea17a03e07396672a9d3e
dsangui <david.sanguinetti@gmail.com>
2013-04-26 19:42:13 +0100

    main construction

--- src/net/eunainter/r2std2oid/R2stD2oid.java
+++ src/net/eunainter/r2std2oid/R2stD2oid.java
@@ -24,108 +24,102 @@ import org.json.JSONObject;
 import android.os.AsyncTask;
 import android.util.Log;
 
-public class R2stD2oid extends AsyncTask<String, Void, String> {
+public class R2stD2oid extends AsyncTask<RequestR2D2, Void, String> {
 	HandleObservers observers;
-	
-	
+
+	final String TAG = "R2stD2oid";
+
+
 	public R2stD2oid() {
 		observers = new HandleObservers();
 	}
-	
-    @Override
-    protected String doInBackground(String... urls) {
-          
-        // params comes from the execute() call: params[0] is the url.
-        try {
-            return downloadUrl(urls[0]);
-        } catch (IOException e) {
-            return "Unable to retrieve web page. URL may be invalid.";
-        }
-    }
-    
-    
-    // onPostExecute displays the results of the AsyncTask.
-    @Override
-    protected void onPostExecute(String result) {
-//        textView.setText(result);
-    	System.out.println("El result: " + result);
-//    	_window.showMessage(result);
-   }
-    
-    private String downloadUrl(String myurl) throws IOException {
-        InputStream is = null;
-        // Only display the first 500 characters of the retrieved
-        // web page content.
-        String result ="";
-        int len = 33000;
-            
-        try {
-			String paramUsername = "david.sanguinetti@gtinformatica.eu";
-			String paramPassword = "estaumlindodia";
-			
-			HttpPost httpPost = new HttpPost(myurl);
+
+	public void addObserver(RestObserver rObserver) {
+		this.observers.addObserver(rObserver);
+	}
+
+	@Override
+	protected String doInBackground(RequestR2D2... requests) {
+
+		try {
+			return downloadUrl(requests[0]);
+		} catch (IOException e) {
+			return "Error processing request";
+		}
+	}
+
+	// onPostExecute displays the results of the AsyncTask.
+	@Override
+	protected void onPostExecute(String result) {
+		//        textView.setText(result);
+		System.out.println("El result: " + result);
+		//    	_window.showMessage(result);
+	}
+
+	private String downloadUrl(RequestR2D2 myRequest) throws IOException {
+		InputStream is = null;
+		// Only display the first 500 characters of the retrieved
+		// web page content.
+		String result ="";
+		int len = 33000;
+
+		try {			
+			HttpPost httpPost = new HttpPost(myRequest.getUrl());
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			BasicCookieStore cookieStore = new BasicCookieStore();
 			httpClient.setCookieStore(cookieStore);
-			
-			String TAG = "R2estD2roid";
-            
-			JSONObject jsonObj = new JSONObject();
 
-			try {
-			    jsonObj.put("username", paramUsername);
-			    jsonObj.put("password", paramPassword);
-			} catch (JSONException e) {
-			    Log.e(TAG, "JSONException: " + e);
-			}
+
+
+			JSONObject jsonObj = myRequest.getJson();
 
 			List<NameValuePair> postParams = new ArrayList<NameValuePair>();
 			postParams.add(new BasicNameValuePair("json", jsonObj.toString()));
 			HttpGet httpGet = null;
 			try {
 				String contentType = "application/json";
-				
-			    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(postParams);
-			    entity.setContentEncoding(HTTP.UTF_8);
-			    entity.setContentType(contentType);
-			    httpPost.setEntity(entity);
 
-			    httpPost.setHeader("Content-Type",contentType);
-			    httpPost.setHeader("Accept", contentType);
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(postParams);
+				entity.setContentEncoding(HTTP.UTF_8);
+				entity.setContentType(contentType);
+				httpPost.setEntity(entity);
+
+				httpPost.setHeader("Content-Type",contentType);
+				httpPost.setHeader("Accept", contentType);
 			} catch (UnsupportedEncodingException e) {
-			    Log.e(TAG, "UnsupportedEncodingException: " + e);
+				Log.e(TAG, "UnsupportedEncodingException: " + e);
 			}
-			
-			try {
-			    HttpResponse httpResponse = httpClient.execute(httpPost);
-			    HttpEntity httpEntity = httpResponse.getEntity();
 
-			    if (httpEntity != null) {
-			        InputStream ist = httpEntity.getContent();
-			        result = readIt(ist);
-			        Log.i(TAG, "Result: " + result);
-			    }
+			try {
+				HttpResponse httpResponse = httpClient.execute(httpPost);
+				HttpEntity httpEntity = httpResponse.getEntity();
+
+				if (httpEntity != null) {
+					InputStream ist = httpEntity.getContent();
+					result = readIt(ist);
+					Log.i(TAG, "Result: " + result);
+				}
 			} catch (ClientProtocolException e) {
-			    Log.e(TAG, "ClientProtocolException: " + e);
+				Log.e(TAG, "ClientProtocolException: " + e);
 			} catch (IOException e) {
-			    Log.e(TAG, "IOException: " + e);
+				Log.e(TAG, "IOException: " + e);
 			}
 
 			return result;
-			
-        } finally {
-            if (is != null) {
-                is.close();
-            } 
-        }
-    }
-    
- // Reads an InputStream and converts it to a String.
-    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");        
-        char[] buffer = new char[33000];
-        reader.read(buffer);
-        return new String(buffer);
-    }
+
+		} finally {
+			if (is != null) {
+				is.close();
+			} 
+		}
+	}
+
+	// Reads an InputStream and converts it to a String.
+	public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+		Reader reader = null;
+		reader = new InputStreamReader(stream, "UTF-8");        
+		char[] buffer = new char[33000];
+		reader.read(buffer);
+		return new String(buffer);
+	}
 }
