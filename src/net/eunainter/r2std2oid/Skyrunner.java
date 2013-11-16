05bcddbc7e373214a354498a3ac687b31c232189
dsangui <david.sanguinetti@gmail.com>
2013-11-16 16:47:12 +0000

    slight improvements

--- src/net/eunainter/r2std2oid/Skyrunner.java
+++ src/net/eunainter/r2std2oid/Skyrunner.java
@@ -9,13 +9,9 @@ public class Skyrunner {
 	
 	
 	// Map of requests
-	private HashMap<Enum, Integer> mRequestIds;
-	
-	public HashMap<Enum, Integer> getmRequestIds() {
-		return mRequestIds;
-	}
+	private HashMap<Integer, Integer> mRequestIds;
 
-	public void setmRequestIds(HashMap<Enum, Integer> mRequestIds) {
+	public void setmRequestIds(HashMap<Integer, Integer> mRequestIds) {
 		this.mRequestIds = mRequestIds;
 	}
 	
@@ -23,19 +19,19 @@ public class Skyrunner {
 	 * Five tag for requests.
 	 * Enums are not easy to implement in switch in Java: traditional method used
 	 */
-	public static final class RequestsTag {
-		public static final int KPOSUM		= 0;
-		public static final int KPOSDOIS	= 1;
-		public static final int KPOSTRES	= 2;
-		public static final int KPOSQUATRO	= 3;
-		public static final int KPOSCINCO	= 4;
-		public static final int SHAREPARAMS	= 5;
+	public static final class RequestTag {
+		public static final int KPOSONE		= 1;
+		public static final int KPOSTWO		= 2;
+		public static final int KPOSTHREE	= 3;
+		public static final int KPOSFOUR	= 4;
+		public static final int KPOSFIVE	= 5;
+		public static final int SHAREPARAMS	= 0;
 	}
 	
 	public Skyrunner() {
 		mR2d2 = new R2stD2oid();	
 		
-		mRequestIds = new HashMap<Enum, Integer>();
+		mRequestIds = new HashMap<Integer, Integer>();
 	}
 	
 	public Skyrunner(RestObserver robserver) {
@@ -53,13 +49,13 @@ public class Skyrunner {
 		mR2d2.addObserver(respobs);
 	}
 	
-	public void sendRequest(RequestR2D2 request, Enum tag) {
+	public void sendRequest(RequestR2D2 request, Integer tag) {
 		createConnection();
 		mR2d2.execute(request);
 		mRequestIds.put(tag, request.getId());
 	}
 	
-	private HashMap<Enum, Integer> getRequestIds() {
+	private HashMap<Integer, Integer> getRequestIds() {
 		
 		return mRequestIds;
 	}
